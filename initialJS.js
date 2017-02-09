b6dfb817b6fc2ea0d0ffd941f990d6e0e3a0b2c8
Eric McCormick <Eric McCormick>
2017-02-09 08:40:45 -0500

    Modified to have the array load as a new object through JS

--- initialJS.js
+++ initialJS.js
@@ -46,11 +46,46 @@ groceryArray[0] = groceryApple;
 groceryArray[1] = groceryBananna;
 groceryArray[2] = groceryKiwi;
 
+//Adding on changes for lab 8 on text changes and total adding
+//var firstItem = document.getElementById('test');
+//firstItem.innerHTML = "Name: " + groceryArray[0].name+ " Price: $"+groceryArray[0].price+".00";
+
+//why wouldn't tag work when I used it for h1?
 //console.log(groceryArray);
+//Calling it outsdie to make sure that sum can also use it
+var itemClass = document.getElementsByClassName("col-lg-6");
 
 groceryArray.forEach(function(groceryFunction){
   console.log("Name: " +groceryFunction.name + " Price: $"+groceryFunction.price+".00");
   sum += groceryFunction.price;
-});
+  //Item
+  var itemName = document.createElement("h4");
 
-console.log("Total Price: $"+sum+".00");
+  itemName.textContent = "The grocery item " + groceryFunction.name + " has a price of $"+groceryFunction.price+".00";
+//it is an array and a square bracket needs to be called
+  itemClass[0].appendChild(itemName);
+  //Price
+  //var itemPrice = document.createElement("p");
+  //itemPrice.textContent = groceryFunction.price;
+  //itemName.appendChild(itemPrice);
+
+});
+//sum
+var totalPrice = document.createElement("h4");
+totalPrice.textContent = "Total Price: $"+sum+".00";
+itemClass[0].appendChild(totalPrice);
+
+var submission = document.getElementsByClassName("btn");
+submission.addEventListner('click', addToObject);
+
+function
+
+
+//console.log("Total Price: $"+sum+".00");
+// x.className = "something";
+
+
+  //more grandular for mine?
+//we can't change but we can add?
+
+//could do a for each to dispaly them
